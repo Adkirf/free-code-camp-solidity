14647119e03953f8c7eed757122b3a8d917eed7c
Adkirf <adkirfuglyape@gmail.com>
2023-04-18 15:59:55 +0200

    first commit

---
+++ hardhat-simple-storage/scripts/deploy.js
@@ -0,0 +1,50 @@
+const { ethers, run, network } = require("hardhat")
+
+async function main() {
+    const SimpleStorageFactory = await ethers.getContractFactory(
+        "SimpleStorage"
+    )
+    console.log("Deploying contract...")
+
+    const simpleStorage = await SimpleStorageFactory.deploy()
+    await simpleStorage.deployed()
+
+    console.log(`deployed contract to: ${simpleStorage.address}`)
+
+    if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+        await simpleStorage.deployTransaction.wait(6)
+        await verify(simpleStorage.address, [])
+    }
+
+    const currentValue = await simpleStorage.retrieve()
+    console.log(`Current Value is: ${currentValue}`)
+
+    const transactionResponse = await simpleStorage.store(7)
+    await transactionResponse.wait(1)
+    const updatedValue = await simpleStorage.retrieve()
+    console.log(`Updated value: ${updatedValue}`)
+}
+
+async function verify(contractAddress, args) {
+    console.log("Verifiying contract")
+
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (err) {
+        if (err.message.toLowerCase().includes("already verified")) {
+            console.log("already verified")
+        } else {
+            console.log(err)
+        }
+    }
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.log(error)
+        process.exit(1)
+    })
